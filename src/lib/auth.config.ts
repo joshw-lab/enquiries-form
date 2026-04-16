75f8f80b2e09dd6d1fbb064af3e282c44025a8cc
Josh Williams <josh.w@completehomefiltration.com.au>
2026-04-16 15:24:46 +0800

    Fix NextAuth Configuration error on Vercel by adding trustHost

--- src/lib/auth.config.ts
+++ src/lib/auth.config.ts
@@ -2,6 +2,7 @@ import Google from 'next-auth/providers/google'
 import type { NextAuthConfig } from 'next-auth'
 
 export const authConfig = {
+  trustHost: true,
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
